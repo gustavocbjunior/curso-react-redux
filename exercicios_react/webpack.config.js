0a83343ad12fa0db691e57cfd0dea2c1f03a450a
Gustavo Barbosa <gustavobarbosaa@gmail.com>
2018-03-14 23:59:52 -0300

    [resolvido] Passando Propriedades para Componentes Filhos (Parte 1)

--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugin: ['transform-object-rest-spread'] 
+                plugins: ['transform-object-rest-spread']
             }
         }]
     }
