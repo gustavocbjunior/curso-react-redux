d1905acfada13344cc561b5c894d6a0506a973a0
Gustavo Barbosa <gustavobarbosaa@gmail.com>
2018-03-14 23:47:56 -0300

    [erro] Passando Propriedades para Componentes Filhos (Parte 1)

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -4,7 +4,7 @@ import Family from './family'
 import Member from './member'
 
 ReactDOM.render(
-    <Family>
-        <Member name='Guilherme' lastName='Silva' />
+    <Family lastName='Silva'>
+        <Member name='Guilherme' />
     </Family>
 , document.getElementById('app'))
