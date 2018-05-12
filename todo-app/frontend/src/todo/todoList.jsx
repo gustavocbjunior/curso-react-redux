3f565805b97c334a07cff476b398504d5c90ed5d
Gustavo Barbosa <gustavobarbosaa@gmail.com>
2018-05-12 14:29:50 -0300

    Exclusão de TODOs

--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -3,8 +3,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import IconButton from '../template/iconButton'
-import { markAsDone } from './todoActions'
-import { markAsPending } from './todoActions'
+import { markAsDone, markAsPending, remove } from './todoActions'
 
 const TodoList = props => {
     const renderRows = () => {
@@ -20,7 +19,7 @@ const TodoList = props => {
                         onClick={() =>
                             props.markAsPending(todo)}></IconButton>
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
-                        onClick={() => props.handleRemove(todo)}></IconButton>
+                        onClick={() => props.remove(todo)}></IconButton>
                 </td>
             </tr>
         ))
@@ -42,6 +41,6 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({list: state.todo.list})
-const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
